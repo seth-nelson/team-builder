bf5fcf2b0fffd236476eae5fc4ec6af11ab1b98e
Seth Nelson <seth.nelson@outlook.com>
2020-02-20 19:09:53 -0800

    added new files

--- team-builder-app/src/App.js
+++ team-builder-app/src/App.js
@@ -3,22 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [members, addMembers] = useState([
+    {id: 0, name: 'Seth', email: 'codeguy@programmer.net', role: 'student'},
+    {id: 1, name: 'Bre', email: 'imbrehello@ilikechocolate.co', role: 'Photogtapher'},
+    {id: 2, name: 'Daisy', email: 'iplayfetch.dog', role: 'Ball Catcher'},
+  ]);
+
+  const addMember = (member) => {
+    setMembers([...members, {...member, id: Date.now() }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Link to='/'>Home Base</Link>
+      <Link to='/add'>Add Member</Link>
+      <Route 
+        path='/add' 
+        render={(props) => 
+          <Form 
+            sumbitMember={addMember} 
+            buttonText='Recruit Teammate' 
+          />} 
+      />
+      <Route 
+        exact path='/' 
+        render={(props) => 
+          people.map((member, index) => 
+            <Card 
+              key={index} 
+              member={member} 
+            />)} 
+      />
+      <Route
+          Path='/edit/:id'
+          render={(props) => {
+            const member = member.find((person) => member.id.toString() === props.match.params.id);
+            return <Form {...props} sumbitMember={editMember} buttonText='Edit Teammate' />;
+          }}
+      />
     </div>
   );
 }
